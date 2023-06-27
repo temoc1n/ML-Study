3a38fc3c4c1d8fec4129ea5975e9fba1c99814c7
Duarte Miranda <Duarte@Duartes-MacBook-Pro.local>
2023-06-28 00:45:57 +0200

    Stacking Dense nonlinear Layers with Keras

---
+++ NeuralNetworks/StackingDenseLayersKeras.py
@@ -0,0 +1,18 @@
+from tensorflow import keras
+import numpy as np
+from tensorflow.python.keras import layers, Sequential
+
+
+
+
+model = Sequential([
+    layers.Dense(units=4, activation='relu', input_shape=[11]), #Input Layer and First Hidden Layer
+    layers.Dense(units=3, activation='relu'),   # Second Hidden Layer
+    layers.Dense(units=1), # Output Layer
+])
+
+X = np.random.rand(10, 11)
+
+Y = model.predict(X)
+
+print(Y)
